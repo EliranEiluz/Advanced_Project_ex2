e5e5d962c4f987aeacd780264403880e94bf1a57
EliranEiluz <eliranfr1@gmail.com>
2022-05-12 12:44:43 +0300

    changes

--- PigeOnlineWebAPI/IPigeOnlineService.cs
+++ PigeOnlineWebAPI/IPigeOnlineService.cs
@@ -4,15 +4,15 @@ namespace PigeOnlineWebAPI;
 
 public interface IPigeOnlineService
 {
-    async Task<Chat> GetChatByUsername(string currentUser, string username);
-    async Task<List<User>> GetContactsByUserName(string currentUser);
+    Task<Chat> GetChatByUsername(string currentUser, string username);
+    Task<List<User>> GetContactsByUserName(string currentUser);
     void AddNewContact(string currentUser, string newUser);
-    async Task<User> GetContactByUsername(string username);
+    Task<User> GetContactByUsername(string username);
     void UpdateContactByUsername(string username);
     void DeleteContactByUsername(string username);
-    async Task<List<Message>> GetMessagesByUsername(string currentUser, string username);
+    Task<List<Message>> GetMessagesByUsername(string currentUser, string username);
     void CreateMessageByUsername(string currentUser, string username, string message);
-    async Task<Message> GetMessageById(int messageID);
+    Task<Message> GetMessageById(int messageID);
     void UpdateMessageById(int messageID, string newContent);
     void DeleteMessageById(int messageID);
     void SendInvitation(string currentUser, string username, string url);
